9e0365b0333646a868852d1c67f23ae2a9f8b739
Harish Ravi <harishamudha@gmail.com>
2019-07-16 12:39:31 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class='flex flex-column'>
+    <header id="nav" class='flex justify-between items-center'>
+      <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/search">search</router-link> -->
+      <div>
+        
+      </div>
+      <div></div>
+    </header>
+    <section id='action-area' class='flex flex-auto justify-center'>
+      <div class="w-60 h-100 bg-gray">
+        <router-view/>
+      </div>
+    </section>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'root',
+
+}
+</script>
+
+
+<style src='./assets/bts.css'>
+
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    sample
+  </div>
+</template>
+
+
+
+
+
+<style>
+/* header main layout*/
+#app{
+  position: fixed;
+  left:0;
+  right:0;
+  bottom:0;
+  top:0;
+}
+#nav{
+  height: 65px;
+}
+#action-area{
+  height: calc(100% - 65px);
+}
+/* --- end -- */
+
+
+</style>
+
